68fc101d3017a1d0bc82a2c4be6f3d8442eb017c
Keyur Golani <keyurrgolani@gmail.com>
2016-11-06 16:41:28 -0800

    Passport Done. Performance testing remaining

--- test/login_user_test.js
+++ test/login_user_test.js
@@ -1,48 +1,25 @@
 
 var expect = require("chai").expect;
-var request = require("request");
-var sjcl = require("sjcl");
+var account_services = require('../services/account_services.js');
 
-var randomString = function(length) {
-	var generatedString = "";
-	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-	for (var i = 0; i < length; i++) {
-		generatedString += possible.charAt(Math.floor(Math.random() * possible.length));
-	}
-	return generatedString;
-};
-
-var passwordpassword = randomString(25);
 
 describe('eBay Login Tests', function() {
 	it('Valid User and Password Test', function(done) {
-		request({
-			url : "http://127.0.0.1:3000/signin",
-			method : "POST",
-			json : true,
-			body : {
-				"userID" : sjcl.encrypt(passwordpassword, "jondoe"),
-				"password" : sjcl.encrypt(passwordpassword, "password123"),
-				"passwordpassword" : passwordpassword
-			}
-		}, function(err, res, body) {
-			expect(body.valid).to.equal(true);
+		account_services.authenticate({
+			'username' : 'keyur'
+			'password' : 'admin123'
+		}, function(reply) {
+			expect(reply.valid).to.equal(true);
 			done();
 		});
 	});
 	
 	it('Invalid User and Password Test', function(done) {
-		request({
-			url : "http://127.0.0.1:3000/signin",
-			method : "POST",
-			json : true,
-			body : {
-				"userID" : sjcl.encrypt(passwordpassword, "jondoe"),
-				"password" : sjcl.encrypt(passwordpassword, "something123"),
-				"passwordpassword" : passwordpassword
-			}
-		}, function(err, res, body) {
-			expect(body.valid).to.equal(false);
+		account_services.authenticate({
+			'username' : 'key'
+			'password' : 'admin'
+		}, function(reply) {
+			expect(reply.valid).to.equal(false);
 			done();
 		});
 	});
